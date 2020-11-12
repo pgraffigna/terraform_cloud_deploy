43e399321610561ec202a72c71f4c8a5b7bf6b63
Pablo Graffigna <pabloggraffigna@gmail.com>
2020-11-12 16:47:35 -0300

    actualización README

--- 02-server_deploy.tf
+++ 02-server_deploy.tf
@@ -1,9 +1,8 @@
-resource "upcloud_server" "ubuntu-01" {
-  hostname = "terraform"
+resource "upcloud_server" "poste" {
+  hostname = "poste"
   count = 1
   zone = "us-nyc1"
   plan = "1xCPU-1GB"
-  ipv6 = false
 
   storage_devices {
     size = 25
@@ -21,9 +20,9 @@ resource "upcloud_server" "ubuntu-01" {
   }
 
   login {
-    user = "USUARIO"
+    user = "quentin"
     keys = [
-      "SSH PUBLIC KEY",
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCu6iQ1jqQqgeMT2g41kBK1sHlbzdN6RcUrzT1+n+QSd7CV+PkjNyvMzN6w9TlM3AiGznxVk1yD36SjYSnabN6KWy9c+q63zu+lHykeprovL4pDooYLFfPXRXIe3KinpY4WLQH0qDi+xNrDUwn1hy8MzLne8hScOOWsJXY9+3OCMsF1Tj9B/ggvvmhyLQkEx67cLsbGxZLIfpodzNdyLF5U2FG8RE2N2L81o31Uasyn1nCBSfDVUNxInrnCRgNpztloGZ2j8LQFeBIjd4gsKOrZICqTTBivQlxmi7P7Wj4Nuvr3AfK1gZX1utqH5I93m/WLotveBS/3VPWlgznLHa1/ quentin@PC-fuckoff",
     ]
     create_password = false
   }
@@ -31,8 +30,8 @@ resource "upcloud_server" "ubuntu-01" {
   connection {
     host        = self.network_interface[0].ip_address
     type        = "ssh"
-    user        = "USUARIO"
-    private_key = file("SSH PRIVATE KEY")
+    user        = "quentin"
+    private_key = file("id_rsa")
   }
 
   provisioner "remote-exec" {
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # terraform_upcloud_deploy
 
-Archivos Terraform para desplegar un servidor básico en Upcloud + instalar docker y docker-compose.
+Terraform para desplegar un servidor Ubuntu 20.04 en Upcloud + instalación de docker y docker-compose.
 
 ---
 
